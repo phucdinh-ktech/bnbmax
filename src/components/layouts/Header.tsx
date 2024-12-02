fe7a96de0084545a10deaf3f480e71c71405da8b
hautv140802 <hautv1408@gmail.com>
2024-12-02 14:30:33 +0700

    fix: more icon

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,4 +1,5 @@
 import { Button } from "antd";
+import clsx from "clsx";
 
 import svgs from "@/assets/svgs";
 
@@ -39,24 +40,25 @@ const Header = () => {
           </ul>
         </div>
 
-        {isLogin ? (
-          <div className="flex justify-end items-center gap-2">
-            <Button className="border-styles-disable text-xs h-[32px] lg:h-[48px] md:text-sm lg:text-base font-medium whitespace-nowrap px-6 lg:px-10 py-2 md:py-2.5 lg:py-3 rounded-md md:rounded-lg lg:rounded-xl duration-300 bg-gray-400 hover:!bg-slate-400 !text-white">
-              0xB3D70...dd990
-            </Button>
-            <div className="block lg:hidden size-[32px] p-2">
-              <img
-                src={svgs.moreVer}
-                alt="more-icons"
-                className="w-full h-full"
-              />
-            </div>
-          </div>
-        ) : (
-          <Button className="border-styles-disable text-xs h-[32px] lg:h-[48px] md:text-sm lg:text-base font-medium whitespace-nowrap px-6 lg:px-10 py-2 md:py-2.5 lg:py-3 rounded-md md:rounded-lg lg:rounded-xl duration-300 bg-secondary hover:!bg-primary !text-white ">
-            Connect wallet
+        <div className="flex justify-end items-center gap-2">
+          <Button
+            className={clsx(
+              "border-styles-disable text-xs h-[32px] lg:h-[48px] md:text-sm lg:text-base font-medium whitespace-nowrap px-6 lg:px-10 py-2 md:py-2.5 lg:py-3 rounded-md md:rounded-lg lg:rounded-xl duration-300",
+              isLogin
+                ? "bg-gray-400 hover:!bg-slate-400 !text-white"
+                : "bg-secondary hover:!bg-primary !text-white"
+            )}
+          >
+            {isLogin ? "0xB3D70...dd990" : "Connect wallet"}
           </Button>
-        )}
+          <div className="block lg:hidden size-[32px] p-2">
+            <img
+              src={svgs.moreVer}
+              alt="more-icons"
+              className="w-full h-full"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
