169a0e71681e90c3d8ce8d31f2ef8998b2acdfa3
hautv140802 <hautv1408@gmail.com>
2024-12-02 14:12:56 +0700

    fix: font family header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,8 +1,10 @@
 import { Button } from "antd";
+import clsx from "clsx";
 
 import svgs from "@/assets/svgs";
 
 const Header = () => {
+  const isLogin = false;
   return (
     <div className="w-full px-5 lg:px-10 z-40">
       <div className="max-w-[1280px] mx-auto flex justify-between items-center h-20 md:h-28">
@@ -14,24 +16,39 @@ const Header = () => {
             <img src={svgs.bnbLogoMobile} className="w-full h-full" />
           </a>
 
-          <ul className="hidden lg:flex items-center gap-6 xl:gap-16 xl:p-4 text-white">
+          <ul className="hidden lg:flex items-center gap-6 xl:gap-16">
             <li className="text-base hover:text-secondary-hover">
-              <a href="#">Buy coin</a>
+              <a href="#" className="p-4 text-white">
+                Buy coin
+              </a>
             </li>
             <li className="text-base hover:text-secondary-hover">
-              <a href="#">Reward</a>
+              <a href="#" className="p-4 text-white">
+                Reward
+              </a>
             </li>
             <li className="text-base hover:text-secondary-hover">
-              <a href="#">Referral</a>
+              <a href="#" className="p-4 text-white">
+                Referral
+              </a>
             </li>
             <li className="text-base  hover:text-secondary-hover">
-              <a href="#">Levels</a>
+              <a href="#" className="p-4 text-white">
+                Levels
+              </a>
             </li>
           </ul>
         </div>
 
-        <Button className="border-styles-disable bg-secondary hover:!bg-primary !text-white text-xs h-[32px] lg:h-[48px] md:text-sm lg:text-base font-medium whitespace-nowrap px-6 lg:px-10 py-2 md:py-2.5 lg:py-3 rounded-md md:rounded-lg lg:rounded-xl duration-300">
-          Connect wallet
+        <Button
+          className={clsx(
+            "border-styles-disable text-xs h-[32px] lg:h-[48px] md:text-sm lg:text-base font-medium whitespace-nowrap px-6 lg:px-10 py-2 md:py-2.5 lg:py-3 rounded-md md:rounded-lg lg:rounded-xl duration-300",
+            isLogin
+              ? "bg-gray-400 hover:!bg-slate-400 !text-white"
+              : "bg-secondary hover:!bg-primary !text-white "
+          )}
+        >
+          {isLogin ? "0xB3D70...dd990" : "Connect wallet"}
         </Button>
       </div>
     </div>
