c4be559594c3fa064f6f696dd911832c01652f32
phucdinh-ktech <ngocphuc@k-tech.net.vn>
2024-12-04 10:40:02 +0700

    feat: add husky pre-commit

--- bnbmax_fe/vite.config.js
+++ bnbmax_fe/vite.config.js
@@ -4,18 +4,18 @@ import { defineConfig } from "vite";
 const __dirname = path.resolve();
 // https://vite.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@/": path.resolve(__dirname, "./src/"),
-            "@/constants": path.resolve(__dirname, "./src/constants/"),
-            "@/components": path.resolve(__dirname, "./src/components/"),
-            "@/pages": path.resolve(__dirname, "./src/pages/"),
-            "@/types": path.resolve(__dirname, "./src/types/"),
-            "@/apis": path.resolve(__dirname, "./src/apis/"),
-            "@/assets": path.resolve(__dirname, "./src/assets/"),
-            "@/libs": path.resolve(__dirname, "./src/libs/"),
-            "@/utils": path.resolve(__dirname, "./src/utils/"),
-        },
+  resolve: {
+    alias: {
+      "@/": path.resolve(__dirname, "./src/"),
+      "@/constants": path.resolve(__dirname, "./src/constants/"),
+      "@/components": path.resolve(__dirname, "./src/components/"),
+      "@/pages": path.resolve(__dirname, "./src/pages/"),
+      "@/types": path.resolve(__dirname, "./src/types/"),
+      "@/apis": path.resolve(__dirname, "./src/apis/"),
+      "@/assets": path.resolve(__dirname, "./src/assets/"),
+      "@/libs": path.resolve(__dirname, "./src/libs/"),
+      "@/utils": path.resolve(__dirname, "./src/utils/"),
     },
-    plugins: [react()],
+  },
+  plugins: [react()],
 });
