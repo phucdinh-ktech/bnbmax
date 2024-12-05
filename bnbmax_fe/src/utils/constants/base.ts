808fd24fa06ad70ae98fd3cb9ac3fe03006a633c
hautv140802 <hautv1408@gmail.com>
2024-12-05 10:28:49 +0700

    fix: env fe

---
+++ bnbmax_fe/src/utils/constants/base.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = import.meta.env.VITE_BASE_URL_DEV;
+export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL_DEV;
--- bnbmax_fe/src/utils/constants/urls.ts
+++ bnbmax_fe/src/utils/constants/urls.ts
@@ -1,5 +1,3 @@
-export const API_URL = "http://localhost:5632/api";
-
 export const urls = {
   USERS: "/users",
   POINTS: "/points",
